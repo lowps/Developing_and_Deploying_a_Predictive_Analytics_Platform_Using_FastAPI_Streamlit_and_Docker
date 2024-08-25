3fda7dac2e2372ab1f620749399ec5a3e6829dd8
Lowps <145309985+lowps@users.noreply.github.com>
2024-08-25 11:41:25 -0400

    iii_create_API.py completed. This file contains FastAPI logic

---
+++ empirical/src/iii_create_API.py
@@ -0,0 +1,105 @@
+import os
+import sys
+import joblib
+import pandas as pd
+import numpy as np
+from fastapi import FastAPI
+from pydantic import BaseModel
+import uvicorn
+
+
+sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
+from utils.logger import Logger
+from utils.helpers import get_directory_name
+
+absolute_path = 'Developing_and_Deploying_a_Predictive_Analytics_Platform_Using_FastAPI_Streamlit_and_Docker/empirical/src/iii_create_API.py'
+inspector_gadget = get_directory_name(absolute_path)
+inspector_gadget = Logger(inspector_gadget)
+
+def initialize_app(title:str, version:str, description:str):
+    '''
+    Initialize the model. This will be loaded at the start of FastAPI model server.
+
+    Parameters:
+    :title: Application title.
+    :version: Application version (should be a string).
+    :description: Application description.
+
+    Returns:
+    FastAPI application instance.
+
+    '''
+    app = FastAPI(title= f'{title}', version= f'{version}', description= f'{description}')
+    return app
+
+
+#initialize FastAPI object in order to utilize "@app" decorators below
+app = initialize_app(title="Car Price Prediction", version="1.0", description="Linear Regression model is used for predictive analytics.")
+
+
+def load_model(file_path: str, mmap_mode:str):
+    """
+    Load a model from a specified file path. This function call uses joblib to deserialize and load the model stored in the '.pkl' file.
+    located at a specific directory on your system. This file is expected to be a saved model, likely serialized using 'joblib'.
+
+    Parameters:
+    :file_path: The path to the file containing the serialized model.
+    :mmap_mode: Specifies how the file should be loaded into memory, particularly useful for large datasets or models. Here are the options:
+        None: This is the default and will load the file into memory normally.
+        'r': Opens the file in read-only mode. This is useful when you don't need to modify the data and want to avoid loading the entire file into memory.
+        'r+': Opens the file in read-write mode without loading it entirely into memory. Changes can be made to the data, but they'll be written back to the file.
+        'w+': Opens the file in read-write mode but truncates the file to zero length. It's generally not used unless you're rewriting the file.
+        'c': Opens the file in copy-on-write mode. Modifications to the data are not written back to the original file but to a copy in memory.
+
+    :model: The result of 'joblib.load' is assigned to the var 'model'. This variable now holds the deserialized model object that was saved previously.
+
+    Returns:
+    :model: The deserialized model object.
+    """
+    if not os.path.isfile(file_path):
+        inspector_gadget.get_log().error(f"load_model() unsuccessful. The file at {file_path} does not exist.")
+        raise FileNotFoundError(f"The file at {file_path} does not exist.")
+        
+    model = joblib.load(file_path)
+    return model
+
+
+class Data(BaseModel):
+    name:str
+    automaker:str
+    year:int
+    miles_driven:int
+    fuel_type:str
+
+
+@app.get('/') #These decorators are used to register route handlers with the FastAPI application instance
+@app.get('/home') #FastAPI, the @app decorators are used to define routes, and they need to be applied to an instance of the FastAPI app
+def read_home():
+    '''
+    Home endpoint which can be used to test the availability of the    application.
+    
+    '''
+    return {'message': 'System is healthy'}
+
+
+@app.post("/predict") #These decorators are used to register route handlers with the FastAPI application instance
+def predict(data: Data, model):
+    '''
+    ML API endpoint to predict against the request received from the client.
+    '''
+    result = model.predict(pd.DataFrame(
+        columns=['name','automaker','year','miles_driven','fuel_type'],
+        data=np.array([data.name,data.automaker,data.year,data.miles_driven,data.fuel_type]).reshape(1,5)))[0]
+    return result
+
+
+
+def main():
+    file_path = '/Users/ericklopez/Desktop/Developing_and_Deploying_a_Predictive_Analytics_Platform_Using_FastAPI_Streamlit_and_Docker/empirical/data/final/LinearRegressionModel.pkl'
+    model = load_model(file_path, 'r')
+    uvicorn.run("iii_create_API:app", host="127.0.0.1", port=8000, reload=True)
+
+
+
+if __name__ == '__main__':
+    main()
